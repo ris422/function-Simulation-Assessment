644527ad3c6a1070cb4a5e2ae252ce571484a749
Rishabh Pal <pal.rishabh5255@gmail.com>
2024-04-19 23:59:35 +0530

    map function

---
+++ Problems/map.js
@@ -0,0 +1,7 @@
+export default function map(items, iteratee) {
+    let results = [];
+    for (let index = 0; index<items.length; index++) {
+      results.push(iteratee(items[index]));
+    }
+    return results;
+  }
